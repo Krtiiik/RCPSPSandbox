ab3da5035d7b825b140a46bde9277edc9f2094a2
Lukas Nedbalek <lukas.nedbalek@email.cz>
2023-08-27 20:05:35 +0200

    Add temporary basic main script

---
+++ rcpsp_sandbox/main.py
@@ -0,0 +1,25 @@
+import sys
+
+from instances.io import parse_psplib, serialize_json
+from instances.drawing import draw_instance_graph
+from instances.utils import print_error
+
+
+def main():
+    if len(sys.argv) != 2:
+        print_error("Invalid command arguments count")
+        if len(sys.argv) == 1:
+            filename = input("Enter input file name: ")
+        else:
+            return
+    else:
+        filename = sys.argv[1]
+
+    instance = parse_psplib(filename, name_as="Test")
+    draw_instance_graph(instance, block=True)
+    serialize_json(instance, "super_cupr_output.json")
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
