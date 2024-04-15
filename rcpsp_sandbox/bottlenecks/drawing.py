773b675f0824396df8b8b1ae64d5495d1bfbdd1e
Lukas Nedbalek <lukas.nedbalek@email.cz>
2024-04-15 19:22:41 +0200

    Update solution drawing

--- rcpsp_sandbox/bottlenecks/drawing.py
+++ rcpsp_sandbox/bottlenecks/drawing.py
@@ -188,7 +188,7 @@ def plot_solution(solution: Solution,
 
 
 def __compute_height_ratios(solution, horizon):
-    interval_panel_height = [math.ceil(1.5*__compute_max_interval_overlap(list(__build_intervals(solution))))]
+    interval_panel_height = [3+math.ceil(1.5*__compute_max_interval_overlap(list(__build_intervals(solution))))]
     resource_panel_heights = [1+(max(c for s, e, c in compute_resource_availability(r, solution.instance, horizon)) // 5)
                               for r in solution.instance.resources]
     return interval_panel_height + resource_panel_heights
