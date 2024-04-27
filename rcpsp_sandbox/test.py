d50bae58cdaafedc271701fcd428ffe2d6ba110a
Lukas Nedbalek <lukas.nedbalek@email.cz>
2024-04-27 21:40:54 +0200

    Update experiment caching

--- rcpsp_sandbox/test.py
+++ rcpsp_sandbox/test.py
@@ -1,4 +1,5 @@
 import os.path
+import pickle
 import random
 
 import numpy as np
@@ -20,7 +21,7 @@ DATA_DIRECTORY_STRUCTURE = {
     'evaluations_kpis_location': os.path.join(DATA_DIRECTORY, 'evaluations_kpis'),
 }
 PLOT_DIRECTORY = os.path.join('..', 'plots')
-
+KPIS_PICKLE_FILENAME = os.path.join(DATA_DIRECTORY, 'kpis.pickle')
 
 def main():
     random.seed(42)
@@ -34,113 +35,71 @@ def main():
     #
     # exit()
 
-    # instances = list(experiment_instances)
-    # instances = [
-    #     "instance01",
-    #     "instance01_1",
-    #     "instance01_2",
-    #     "instance01_3",
-    #     "instance01_4",
-    # ]
-    # instances = [
-    #     "instance02",
-    #     "instance02_1",
-    #     "instance02_2",
-    #     "instance02_3",
-    #     "instance02_4",
-    # ]
-    # instances = [
-    #     "instance03",
-    #     "instance03_1",
-    #     "instance03_2",
-    #     "instance03_3",
-    #     "instance03_4",
-    # ]
-    # instances = [
-    #     "instance04",
-    #     "instance04_1",
-    #     "instance04_2",
-    #     "instance04_3",
-    #     "instance04_4",
-    # ]
-    # instances = [
-    #     "instance05",
-    #     "instance05_1",
-    #     "instance05_2",
-    #     "instance05_3",
-    #     "instance05_4",
-    # ]
-    # instances = [
-    #     "instance06",
-    #     "instance06_1",
-    #     "instance06_2",
-    #     "instance06_3",
-    #     "instance06_4",
-    # ]
-    # instances = [
-    #     "instance07",
-    #     "instance07_1",
-    #     "instance07_2",
-    #     "instance07_3",
-    #     "instance07_4",
-    # ]
-    instances = [
-        "instance08",
-        "instance08_1",
-        "instance08_2",
-        "instance08_3",
-        "instance08_4",
-    ]
+    if os.path.exists(KPIS_PICKLE_FILENAME):
+        with open(KPIS_PICKLE_FILENAME, "rb") as f:
+            instance_evaluations_kpis = pickle.load(f)
+    else:
+        instances = []
+        # instances += ["instance01", "instance01_1", "instance01_2", "instance01_3", "instance01_4"]
+        # instances += ["instance02", "instance02_1", "instance02_2", "instance02_3", "instance02_4"]
+        # instances += ["instance03", "instance03_1", "instance03_2", "instance03_3", "instance03_4"]
+        # instances += ["instance04", "instance04_1", "instance04_2", "instance04_3", "instance04_4"]
+        # instances += ["instance05", "instance05_1", "instance05_2", "instance05_3", "instance05_4"]
+        # instances += ["instance06", "instance06_1", "instance06_2", "instance06_3", "instance06_4"]
+        # instances += ["instance07", "instance07_1", "instance07_2", "instance07_3", "instance07_4"]
+        instances += ["instance08", "instance08_1", "instance08_2", "instance08_3", "instance08_4"]
+        # instances = list(experiment_instances)
 
-    # TODO auac -> auau Utilization
+        instance_evaluations_kpis = dict()
+        with ExperimentManager(**DATA_DIRECTORY_STRUCTURE) as manager:
+            for instance_name in instances:
+                instance = manager.load_base_instance(instance_name)
+                from solver.solver import Solver; plot_solution(Solver().solve(instance), block=False, save_as=os.path.join('..', 'insts', instance_name+'.png'))
 
-    instance_evaluations_kpis = dict()
-    with ExperimentManager(**DATA_DIRECTORY_STRUCTURE) as manager:
-        for instance_name in instances:
-            instance = manager.load_base_instance(instance_name)
-            from solver.solver import Solver; plot_solution(Solver().solve(instance), block=False, save_as=os.path.join('..', 'insts', instance_name+'.png'))
+                evaluations = evaluate_algorithms(
+                    instance,
+                    [
+                        (TimeVariableConstraintRelaxingAlgorithm(), {
+                            "max_iterations": [1, 2, 3, 4, 5, 6],
+                            "relax_granularity": [1],
+                            "max_improvement_intervals": [1, 2, 3, 4, 5, 6],
+                            "interval_sort": ["improvement"]
+                        }),
+                        (TimeVariableConstraintRelaxingAlgorithm(), {
+                            "max_iterations": [1, 2, 3, 4, 5, 6],
+                            "relax_granularity": [1],
+                            "max_improvement_intervals": [1, 2, 3, 4, 5, 6],
+                            "interval_sort": ["time"]
+                        }),
+                        (MetricsRelaxingAlgorithm(), {
+                            "metric": ["auac"],
+                            "granularity": [4, 8],
+                            "convolution_mask": ["pre1", "around"],
+                            "max_iterations": [1, 2, 3, 4, 5, 6],
+                            "max_improvement_intervals": [1, 2, 3, 4],
+                            "capacity_addition": [6, 8, 10],
+                        }),
+                        (MetricsRelaxingAlgorithm(), {
+                            "metric": ["mrur"],
+                            "granularity": [4, 8],
+                            "convolution_mask": ["pre1", "around"],
+                            "max_iterations": [1, 2, 3, 4, 5, 6],
+                            "max_improvement_intervals": [1, 2, 3, 4],
+                            "capacity_addition": [6, 8, 10],
+                        }),
+                    ],
+                    cache_manager=manager,
+                )
+                evaluations_kpis = compute_evaluation_kpis(evaluations, 5, 1)
 
-            evaluations = evaluate_algorithms(
-                instance,
-                [
-                    (TimeVariableConstraintRelaxingAlgorithm(), {
-                        "max_iterations": [1, 2, 3, 4, 5, 6],
-                        "relax_granularity": [1],
-                        "max_improvement_intervals": [1, 2, 3, 4, 5, 6],
-                        "interval_sort": ["improvement"]
-                    }),
-                    (TimeVariableConstraintRelaxingAlgorithm(), {
-                        "max_iterations": [1, 2, 3, 4, 5, 6],
-                        "relax_granularity": [1],
-                        "max_improvement_intervals": [1, 2, 3, 4, 5, 6],
-                        "interval_sort": ["time"]
-                    }),
-                    (MetricsRelaxingAlgorithm(), {
-                        "metric": ["auac"],
-                        "granularity": [4, 8],
-                        "convolution_mask": ["pre1", "around"],
-                        "max_iterations": [1, 2, 3, 4, 5, 6],
-                        "max_improvement_intervals": [1, 2, 3, 4],
-                        "capacity_addition": [6, 8, 10],
-                    }),
-                    (MetricsRelaxingAlgorithm(), {
-                        "metric": ["mrur"],
-                        "granularity": [4, 8],
-                        "convolution_mask": ["pre1", "around"],
-                        "max_iterations": [1, 2, 3, 4, 5, 6],
-                        "max_improvement_intervals": [1, 2, 3, 4],
-                        "capacity_addition": [6, 8, 10],
-                    }),
-                ],
-                cache_manager=manager,
-            )
-            evaluations_kpis = compute_evaluation_kpis(evaluations, 5, 1)
+                # manager.save_modified_instances(evaluation.modified_instance for evaluation in flatten(evaluations))
+                # manager.save_evaluations(flatten(evaluations))
+                manager.save_evaluations_kpis(flatten(evaluations_kpis))
 
-            # manager.save_modified_instances(evaluation.modified_instance for evaluation in flatten(evaluations))
-            # manager.save_evaluations(flatten(evaluations))
-            manager.save_evaluations_kpis(flatten(evaluations_kpis))
+                instance_evaluations_kpis[instance_name] = evaluations_kpis
 
-            instance_evaluations_kpis[instance_name] = evaluations_kpis
+        with open(KPIS_PICKLE_FILENAME, "wb") as f:
+            pickle.dump(instance_evaluations_kpis, f)
 
     plot_evaluations(instance_evaluations_kpis, value_axes=("cost", "improvement"), pareto_front=True)
     plot_evaluations(instance_evaluations_kpis, value_axes=("improvement", "schedule difference"), pareto_front=True)
