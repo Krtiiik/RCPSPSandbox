b92f2bac3537f9f31c44bc7ad264b307162596ff
Lukas Nedbalek <lukas.nedbalek@email.cz>
2024-04-30 16:12:47 +0200

    Update result table data

--- rcpsp_sandbox/experiments.py
+++ rcpsp_sandbox/experiments.py
@@ -90,10 +90,9 @@ def get_evaluations_kpis(args: argparse.Namespace):
     return evaluations_kpis
 
 
-def compute_statistics(evaluations_kpis: dict[str, list[list[EvaluationKPIs]]], args):
-    if args.aggregate:
-        evaluations_kpis = group_evaluations_kpis_by_instance_type(evaluations_kpis, args.scale)
-
+def compute_statistics(evaluations_kpis: dict[str, list[list[EvaluationKPIs]]],
+                       aggregated_evaluations_kpis: dict[str, list[list[EvaluationKPIs]]],
+                       args):
     def extract_kpis(_kpis):
         return [[
             _e_kpi.improvement,
@@ -133,99 +132,132 @@ def compute_statistics(evaluations_kpis: dict[str, list[list[EvaluationKPIs]]], 
     improving_inst_mins = np.min(improving_mins, axis=1)
     improving_inst_avgs = np.average(improving_avgs, axis=1)
 
-    # Found improvements
-    total_improved_instances = np.sum(inst_maxs[:, I_improvement] > 0)
-    ssira_improved_instances = np.sum(maxs[:, i_ssira, I_improvement] > 0)
-    iira_improved_instances = np.sum(maxs[:, i_iira, I_improvement] > 0)
-
-    # Best found improvements
-    ssira_best_improvements = np.sum(inst_maxs[:, I_improvement] == maxs[:, i_ssira, I_improvement])
-    iira_best_improvements = np.sum(inst_maxs[:, I_improvement] == maxs[:, i_iira, I_improvement])
-    both_best_improvements = np.sum(maxs[:, i_ssira, I_improvement] == maxs[:, i_iira, I_improvement])
+    # Found improvement
+    iira_improved = maxs[:, i_iira, I_improvement] > 0
+    iira_improved_aggregated = np.sum(iira_improved.reshape((8, -1)), axis=1)
+    iira_improved_best = inst_maxs[:, I_improvement] == maxs[:, i_iira, I_improvement]
+    ssira_improved = maxs[:, i_ssira, I_improvement] > 0
+    ssira_improved_best = inst_maxs[:, I_improvement] == maxs[:, i_ssira, I_improvement]
+    ssira_improved_aggregated = np.sum(ssira_improved.reshape((8, -1)), axis=1)
 
     # Cost
-    ssira_avg_cost_per_improvement = improving_avgs[:, i_ssira, I_cost] / improving_avgs[:, i_ssira, I_improvement]
-    iira_avg_cost_per_improvement = improving_avgs[:, i_iira, I_cost] / improving_avgs[:, i_iira, I_improvement]
+    avgs_cost_per_improvement = np.array([[np.average(alg_kpis[:, I_cost] / alg_kpis[:, I_improvement]) for alg_kpis in algs_kpis] for algs_kpis in improving_kpis_by_alg_by_inst])
+    ssira_avg_cost_per_improvement = avgs_cost_per_improvement[:, i_ssira]
+    iira_avg_cost_per_improvement = avgs_cost_per_improvement[:, i_iira]
 
     # Schedule difference
     inst_ns = np.array([experiment_instances_info[instance]["n"] for instance in evaluations_kpis])
+    ssira_avg_diff = improving_avgs[:, i_ssira, I_schedule_difference]
     ssira_avg_diff_per_job = improving_avgs[:, i_ssira, I_schedule_difference] / inst_ns
+    iira_avg_diff = improving_avgs[:, i_iira, I_schedule_difference]
     iira_avg_diff_per_job = improving_avgs[:, i_iira, I_schedule_difference] / inst_ns
 
-    print("total_improved_instances", ": ", total_improved_instances)
-    print("ssira_improved_instances", ": ", ssira_improved_instances)
-    print("iira_improved_instances",  ": ", iira_improved_instances)
-    print("ssira_best_improvements",  ": ", ssira_best_improvements)
-    print("iira_best_improvements",   ": ", iira_best_improvements)
-    print("both_best_improvements",   ": ", both_best_improvements)
+    # Duration
+    avgs_improvement_per_duration = np.array([[np.average(alg_kpis[:, I_improvement] / alg_kpis[:, I_duration]) for alg_kpis in algs_kpis] for algs_kpis in improving_kpis_by_alg_by_inst])
+    ssira_avg_improvement_per_duration = avgs_improvement_per_duration[:, i_ssira]
+    iira_avg_improvement_per_duration = avgs_improvement_per_duration[:, i_iira]
 
-    table_data = [[
-            instance,
-            ssira_avg_diff_per_job[i_instances[instance]],
-            ssira_avg_cost_per_improvement[i_instances[instance]],
-            iira_avg_diff_per_job[i_instances[instance]],
-            iira_avg_cost_per_improvement[i_instances[instance]],
-        ] for instance in evaluations_kpis
-        ]
-    t = tabulate.tabulate(
-        table_data,
-        ["Instance", "SSIRA avg diff", "SSIRA avg cost/improvement", "IIRA avg diff", "IIRA avg cost/improvement"],
-        tablefmt='tsv',
-    )
-    print(t)
+    def do_table(_data, _columns, _name):
+        print(tabulate.tabulate(_data, _columns))
+        with open(os.path.join(RESULTS_DIRECTORY, _name+'.tsv'), "wt") as f:
+            def _row(*_vals): print(*_vals, sep='\t', end='\n', file=f)
+            _row(*_columns)
+            for _row_data in _data:
+                _row(*_row_data)
 
-    with open(os.path.join(RESULTS_DIRECTORY, ('aggregated_data.tsv' if args.aggregate else 'data.tsv')), "wt") as f:
-        def row(*_vals): print(*_vals, sep='\t', end='\n', file=f)
-        row("Instance", "SSIRA avg diff", "IIRA avg diff", "SSIRA avg cost/improvement", "IIRA avg cost/improvement")
-        for instance_data in table_data:
-            row(*instance_data)
+    data_instances = [[
+        f"instance{i+1}*",
+
+        f'{iira_improved_aggregated[i]}/5',
+        (f'{np.sum(iira_improved_best.reshape((8, -1)), axis=1)[i]}/{iira_improved_aggregated[i]}' if iira_improved_aggregated[i] > 0 else " "),
+
+        f'{ssira_improved_aggregated[i]}/5',
+        (f'{np.sum(ssira_improved_best.reshape((8, -1)), axis=1)[i]}/{ssira_improved_aggregated[i]}' if ssira_improved_aggregated[i] > 0 else " "),
+    ] for i in range(len(aggregated_evaluations_kpis))] + [[
+        "Total",
+
+        f'{iira_improved_aggregated.sum()}/{n_instances}',
+        f'{iira_improved_best.sum()}/{iira_improved_aggregated.sum()}',
+
+        f'{ssira_improved_aggregated.sum()}/{n_instances}',
+        f'{ssira_improved_best.sum()}/{ssira_improved_aggregated.sum()}',
+    ]]
+    data_instances_cols = [
+        "Instances",
+        "IIRA improved", "IIRA improved best",
+        "SSIRA improved", "SSIRA improved best",
+    ]
+
+    do_table(data_instances, data_instances_cols, "data_instances")
+
+    data_kpis = [[
+        instance,
+
+        iira_avg_diff[i_instances[instance]],
+        iira_avg_diff_per_job[i_instances[instance]],
+        iira_avg_cost_per_improvement[i_instances[instance]],
+        iira_avg_improvement_per_duration[i_instances[instance]],
+
+        ssira_avg_diff[i_instances[instance]],
+        ssira_avg_diff_per_job[i_instances[instance]],
+        ssira_avg_cost_per_improvement[i_instances[instance]],
+        ssira_avg_improvement_per_duration[i_instances[instance]],
+    ] for instance in evaluations_kpis]
+    data_kpis_cols = [
+        "Instance",
+        "IIRA avg diff", "IIRA avg diff per job", "IIRA avg cost per improvement", "IIRA avg improvement per duration",
+        "SSIRA avg diff", "SSIRA avg diff per job", "SSIRA avg cost per improvement", "SSIRA avg improvement per duration",
+    ]
+
+    do_table(data_kpis, data_kpis_cols, "data_kpis")
 
 
 # noinspection PyTypeChecker
 def plot(evaluations_kpis, args: argparse.Namespace):
-    # Determine whether to save plots to files
     cost_improv = improv_diff = duration_improv = None
+    aggregated_cost_improv = aggregated_improv_diff = aggregated_duration_improv = None
+    # Determine whether to save plots to files
     if args.save_plots:
-        if args.aggregate:
-            cost_improv = os.path.join(RESULTS_DIRECTORY, 'aggregated_cost_improv.pdf')
-            improv_diff = os.path.join(RESULTS_DIRECTORY, 'aggregated_improv_diff.pdf')
-            duration_improv = os.path.join(RESULTS_DIRECTORY, 'aggregated_duration_improv.pdf')
-        else:
-            cost_improv = os.path.join(RESULTS_DIRECTORY, 'cost_improv.pdf')
-            improv_diff = os.path.join(RESULTS_DIRECTORY, 'improv_diff.pdf')
-            duration_improv = os.path.join(RESULTS_DIRECTORY, 'duration_improv.pdf')
+        cost_improv = os.path.join(RESULTS_DIRECTORY, 'cost_improv.pdf')
+        improv_diff = os.path.join(RESULTS_DIRECTORY, 'improv_diff.pdf')
+        duration_improv = os.path.join(RESULTS_DIRECTORY, 'duration_improv.pdf')
+        aggregated_cost_improv = os.path.join(RESULTS_DIRECTORY, 'aggregated_cost_improv.pdf')
+        aggregated_improv_diff = os.path.join(RESULTS_DIRECTORY, 'aggregated_improv_diff.pdf')
+        aggregated_duration_improv = os.path.join(RESULTS_DIRECTORY, 'aggregated_duration_improv.pdf')
 
     cost_improv_kpis = {instance: [pareto_front_kpis(alg_kpis, x="cost", y="improvement") for alg_kpis in algs_kpis] for instance, algs_kpis in evaluations_kpis.items()}
     improv_diff_kpis = {instance: [pareto_front_kpis(alg_kpis, x="improvement", y="schedule difference") for alg_kpis in algs_kpis] for instance, algs_kpis in evaluations_kpis.items()}
     duration_improv_kpis = {instance: [pareto_front_kpis(alg_kpis, x="duration", y="improvement") for alg_kpis in algs_kpis] for instance, algs_kpis in evaluations_kpis.items()}
-
-    if args.aggregate:
-        cost_improv_kpis = group_evaluations_kpis_by_instance_type(cost_improv_kpis, args.scale)
-        improv_diff_kpis = group_evaluations_kpis_by_instance_type(improv_diff_kpis, args.scale)
-        duration_improv_kpis = group_evaluations_kpis_by_instance_type(duration_improv_kpis, args.scale)
+    aggregated_cost_improv_kpis = group_evaluations_kpis_by_instance_type(cost_improv_kpis)
+    aggregated_improv_diff_kpis = group_evaluations_kpis_by_instance_type(improv_diff_kpis)
+    aggregated_duration_improv_kpis = group_evaluations_kpis_by_instance_type(duration_improv_kpis)
 
     # Plotting
-    ncols = 2 if args.aggregate else 8
-    dimensions = (8, 11) if args.aggregate else (35, 24)
-    layout = ({"hspace": 0.50, "wspace": 0.3, "top": 0.97, "bottom": 0.13, "left": 0.10, "right": 0.99} if args.aggregate else
-              {"hspace": 0.25, "wspace": 0.3, "top": 0.98, "bottom": 0.06, "left": 0.03, "right": 0.99})
+    ncols = 8
+    dimensions = (35, 24)
+    layout = {"hspace": 0.25, "wspace": 0.3, "top": 0.98, "bottom": 0.06, "left": 0.03, "right": 0.99}
+    aggregated_ncols = 2
+    aggregated_dimensions = (8, 11)
+    aggregated_layout = {"hspace": 0.50, "wspace": 0.3, "top": 0.97, "bottom": 0.13, "left": 0.10, "right": 0.99}
     labels = ["SSIRA (improvement sort)", "SSIRA (time sort)", "IIRA (AUAU)", "IIRA (MRUR)"]
-    plot_evaluations(cost_improv_kpis, value_axes=("cost", "improvement"), save_as=cost_improv, ncols=ncols, dimensions=dimensions, layout=layout, inverse_order=not args.aggregate, labels=labels)
-    plot_evaluations(improv_diff_kpis, value_axes=("improvement", "schedule difference"), save_as=improv_diff, ncols=ncols, dimensions=dimensions, layout=layout, inverse_order=not args.aggregate, labels=labels)
-    plot_evaluations(duration_improv_kpis, value_axes=("duration", "improvement"), save_as=duration_improv, ncols=ncols, dimensions=dimensions, layout=layout, inverse_order=not args.aggregate, labels=labels)
+    plot_evaluations(cost_improv_kpis, value_axes=("cost", "improvement"), save_as=cost_improv, ncols=ncols, dimensions=dimensions, layout=layout, inverse_order=True, labels=labels)
+    plot_evaluations(improv_diff_kpis, value_axes=("improvement", "schedule difference"), save_as=improv_diff, ncols=ncols, dimensions=dimensions, layout=layout, inverse_order=True, labels=labels)
+    plot_evaluations(duration_improv_kpis, value_axes=("duration", "improvement"), save_as=duration_improv, ncols=ncols, dimensions=dimensions, layout=layout, inverse_order=True, labels=labels)
+    plot_evaluations(aggregated_cost_improv_kpis, value_axes=("cost", "improvement"), save_as=aggregated_cost_improv, ncols=aggregated_ncols, dimensions=aggregated_dimensions, layout=aggregated_layout, inverse_order=False, labels=labels)
+    plot_evaluations(aggregated_improv_diff_kpis, value_axes=("improvement", "schedule difference"), save_as=aggregated_improv_diff, ncols=aggregated_ncols, dimensions=aggregated_dimensions, layout=aggregated_layout, inverse_order=False, labels=labels)
+    plot_evaluations(aggregated_duration_improv_kpis, value_axes=("duration", "improvement"), save_as=aggregated_duration_improv, ncols=aggregated_ncols, dimensions=aggregated_dimensions, layout=aggregated_layout, inverse_order=False, labels=labels)
 
 
 def main(args: argparse.Namespace):
     evaluations_kpis = get_evaluations_kpis(args)
-    compute_statistics(evaluations_kpis, args)
+    aggregated_evaluations_kpis = group_evaluations_kpis_by_instance_type(evaluations_kpis)
+    compute_statistics(evaluations_kpis, aggregated_evaluations_kpis, args)
     plot(evaluations_kpis, args)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--aggregate", action="store_true", default=False, help="Determines whether to aggregate instances by type")
-    parser.add_argument("--scale", action="store_true", default=False, help="Determines whether to scale evaluations KPIs")
-    parser.add_argument("--save_plots", action="store_true", default=True, help="Determines whether to save plots to files")
+    parser.add_argument("--save_plots", action="store_true", default=False, help="Determines whether to save plots to files")
     parser.add_argument("--addition", action="store", default=5, type=int, help="Cost of capacity addition")
     parser.add_argument("--migration", action="store", default=1, type=int, help="Cost of capacity migration")
     main(parser.parse_args())
